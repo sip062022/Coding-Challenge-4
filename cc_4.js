3eb49da204d80a55c0c4bd562ecbf30aef8d8b9d
Sahara Sipple <sahara36@usf.edu>
2025-02-02 10:02:13 -0500

    Task 2 : For Loop

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,14 @@ if (purchaseAmount > 100) { // if statement to apply discount if greater than 10
 }
 
 console.log(`Final amount after discount: $${finalAmount}`); // Displays final purchase amount
+
+// Task 2: For Loop //
+
+let sales = [20, 40, 60, 80, 100]; // Declares the array
+let totalSales = 0; // Declares totalSales variable 
+
+for (let i = 0; i < sales.length; i++) {  // Declares for loop 
+    totalSales += sales[i]   // Says that totalSales are sum of the numbers in the loop
+};
+
+console.log(`Total Sales: $${totalSales}`);  // Displays Total sales amount
